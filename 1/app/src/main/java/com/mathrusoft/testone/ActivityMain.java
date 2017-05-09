0ca65a9fb1c00a3163e359b74225a3a8f330f313
Sharan Angadi <sharan@mathrusoft.com>
2017-05-09 20:11:34 +0530

    1

---
+++ 1/app/src/main/java/com/mathrusoft/testone/ActivityMain.java
@@ -0,0 +1,19 @@
+package com.mathrusoft.testone;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by sharanangadi on 09/05/17.
+ */
+
+public class ActivityMain extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_mains);
+    }
+}
