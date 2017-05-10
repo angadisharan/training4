f29f36c3b4d584d83703842b7b9fd449e6449ea0
Sharan Angadi <sharan@mathrusoft.com>
2017-05-10 20:04:53 +0530

    layotu demo

---
+++ LayoutDemo/app/src/main/java/com/mathrusoft/layoutdemo/ActivityMain.java
@@ -0,0 +1,14 @@
+package com.mathrusoft.layoutdemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ActivityMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.demo_relative_layout);
+    }
+}
+
