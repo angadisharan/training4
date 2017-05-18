afa55c0cf50e5a872a353dfd4e64a33522c46d93
Sharan Angadi <sharan@mathrusoft.com>
2017-05-18 20:18:54 +0530

    nav fragment

---
+++ NavigationBar/app/src/main/java/com/mathrusoft/navigationbar/ActivityMain.java
@@ -0,0 +1,98 @@
+package com.mathrusoft.navigationbar;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.mathrusoft.navigationbar.fragment.FragmentCamera;
+import com.mathrusoft.navigationbar.fragment.FragmentGallery;
+import com.mathrusoft.navigationbar.fragment.FragmentMain;
+import com.mathrusoft.navigationbar.fragment.FragmentSettings;
+import com.mathrusoft.navigationbar.fragment.FragmentSlideshow;
+
+public class ActivityMain extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        replaceFragment(new FragmentMain());
+
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.nav_camera:
+                replaceFragment(new FragmentCamera());
+                break;
+            case R.id.nav_gallery:
+                replaceFragment(new FragmentGallery());
+                break;
+            case R.id.nav_slideshow:
+                replaceFragment(new FragmentSlideshow());
+                break;
+            case R.id.nav_settings:
+                replaceFragment(new FragmentSettings());
+                break;
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_holder, fragment);
+        fragmentTransaction.commit();
+
+    }
+}
---
+++ NavigationBar/app/src/main/java/com/mathrusoft/navigationbar/fragment/FragmentMain.java
@@ -0,0 +1,23 @@
+package com.mathrusoft.navigationbar.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.mathrusoft.navigationbar.R;
+
+/**
+ * Created by sharanangadi on 18/05/17.
+ */
+
+public class FragmentMain extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, null);
+    }
+}
